7cf55f674ef74487581b8d56516cc566fb764f38
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2022-05-27 21:29:15 -0300

    IF/Switch

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        
+//        if hardness == "Soft" {
+//            print(softTime)
+//        } else if hardness == "Medium"{
+//            print(mediumTime)
+//        } else {
+//            print(hardTime)
+//        }
+        
+        switch hardness {
+        case "Soft":
+            print(softTime)
+        case "Medium":
+            print(mediumTime)
+        case "Hard":
+            print(hardTime)
+        default:
+            print("Error")
+        }
+        
+    }
 }
